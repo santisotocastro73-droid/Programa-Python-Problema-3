7154f2fb3c9557af185d53819eb0d95e73dc9493
Santiago Soto <santisoto.castro73@gmail.com>
2026-05-23 16:28:54 -0500

    Se creo la lista de listas osea la matriz

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 print("Mi nombre es Santiago Soto y este es el problema # 3")
+inventario=[["A001","Arroz", 10, 20],
+            ["A002","Frijoles",5,10],
+            ["A003","Azucar", 10, 30],
+            ["A004","Sal", 8, 20],
+            ["A005","Aceite", 15, 25]]
+print(inventario)
