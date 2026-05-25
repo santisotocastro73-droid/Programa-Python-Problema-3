bfa1120d441157e174fcdab71e50793a3a0b522e
Santiago Soto <santisoto.castro73@gmail.com>
2026-05-25 15:27:48 -0500

    aqui se hizo la salida que pide el codigo para resolver el problema

--- script.py
+++ script.py
@@ -13,3 +13,9 @@ print(f"{'Código':<6} {'Nombre':<10} {'Stock Actual':<13} {'Stock Mínimo':<10}
 print("-" * 50)
 for articulo in inventario:
     print(f"{articulo[0]:<6} {articulo[1]:<10} {articulo[2]:<13} {articulo[3]:<10}")
+#ahora generaremos la lista de pedidos para complertar el stock mínimo
+print("\nLista de pedidos para completar el stock mínimo:")
+for articulo in inventario:
+    cantidad= cantidad_a_pedir(articulo[2], articulo[3])
+    if cantidad > 0:
+        print(f"{articulo[0]:<6} {articulo[1]:<10} {cantidad:<13}")
