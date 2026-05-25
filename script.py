2a5a8db45a723e6a386b3d50d7d20949e901ba33
Santiago Soto <santisoto.castro73@gmail.com>
2026-05-25 15:20:08 -0500

    corregi el orden de donde habia ubicado la funcion o modulo

--- script.py
+++ script.py
@@ -4,12 +4,12 @@ inventario=[["A001","Arroz", 10, 20],
             ["A003","Azucar", 10, 30],
             ["A004","Sal", 8, 20],
             ["A005","Aceite", 15, 25]]
-print(f"{'Código':<6} {'Nombre':<10} {'Stock Actual':<13} {'Stock Mínimo':<10}")
-print("-" * 50)
-for articulo in inventario:
-    print(f"{articulo[0]:<6} {articulo[1]:<10} {articulo[2]:<13} {articulo[3]:<10}")
 def cantidad_a_pedir(stock_actual, stock_minimo):
     if stock_actual < stock_minimo:
         return stock_minimo - stock_actual
     else:
         return 0
+print(f"{'Código':<6} {'Nombre':<10} {'Stock Actual':<13} {'Stock Mínimo':<10}")
+print("-" * 50)
+for articulo in inventario:
+    print(f"{articulo[0]:<6} {articulo[1]:<10} {articulo[2]:<13} {articulo[3]:<10}")
